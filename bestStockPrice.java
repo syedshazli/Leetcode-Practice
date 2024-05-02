8955454f4be66f1b6b031c89f5008056832ce20c
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-01 22:30:41 -0400

    Create bestStockPrice.java

---
+++ bestStockPrice.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // find the greatest difference in sorted form
+        // index matters, earlier the index the earlier the day
+
+        // if you buy something early and can sell it later
+        // the max is the difference between the two
+        // brute force method
+
+        
+        int largest = prices[0];
+      
+        int max = 0;
+        for (int i = 1; i < prices.length; i++) {
+          
+            if(prices[i]<largest){
+                largest = prices[i];
+
+
+                //max = prices[i];
+
+
+
+                }
+
+           else if(prices[i]- largest> max){
+            max = prices[i] - largest;
+           }
+
+            }
+
+           return max;  // do something
+        }
+       
+    }
