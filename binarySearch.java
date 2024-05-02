e8e75ed6765ed1facfe41039c888b5c4c6b968ca
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-02 00:06:08 -0400

    Create binarySearch.java

---
+++ binarySearch.java
@@ -0,0 +1,33 @@
+class binarySearch {
+    public int search(int[] nums, int target) {
+        
+        int low = 0;
+        int high = nums.length-1;
+
+        while(low<= high){
+            int middle = low+(high-low)/2;
+            int val = nums[middle];
+
+
+        
+
+       if(val>target){ //middle number exceeded target value, decrease upper bound
+
+            high = middle-1;
+        }
+
+        else if(val<target){//middle number was too low, increase the bound
+            low = middle +1;
+        }
+        else{return middle;}
+
+        }
+
+       
+
+       
+
+        return -1;
+
+    }
+}
