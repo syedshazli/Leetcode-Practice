e02a22a169c7eabe17d8b6e62d55130c318956a1
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-03 18:30:56 -0400

    Read notes

--- invertBinaryTree.java
+++ invertBinaryTree.java
@@ -1,5 +1,42 @@
 /**
 *Notes on Problem
-*
-*
+*My implementation is a Depth First Search (DFS) Approach
+*The implementation uses recursion to find the leaf nodes, which then return the parent node to allow swapping
+* We switch the left and the right of the roots to ivnert the tree properly
 */
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+    
+            if(root == null){return root;}
+            TreeNode leftTree = invertTree(root.left);
+            TreeNode rightTree = invertTree(root.right);
+
+            root.left = rightTree;
+            root.right = leftTree;
+
+            return root;
+
+
+    }
+      
+    
+
+}
