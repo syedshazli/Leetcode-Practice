9dbf431fab5033543ad6cc483f55c3428f511229
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-04 19:08:20 -0400

    redo validParentheses.java

--- validParentheses.java
+++ validParentheses.java
@@ -1,3 +1,12 @@
+/**
+*Notes
+* This is a notroious stack problem. How do we recognize this?
+*  We can notice this by seeing that the answer directly depends on a corresponding element
+*  Each time we have a opening parentheses, push the closing parenthesis to the stack
+*  If the stack is empty or we don't find the closing parenthesis next, we return false
+*  Else, it's true.
+*/
+
 class validParentheses {
     public boolean isValid(String s) {
         
