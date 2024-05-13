80786b68250ea4dcba23d5e77e1d431223edd422
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-13 00:07:03 -0400

    Update twoSumRedo.java

--- files/twoSum/twoSumRedo.java
+++ files/twoSum/twoSumRedo.java
@@ -1,4 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+      //lets try to implement with a better runtime than 
+      // O(n^2) do this with hashmaps maybe?
         }
 }
