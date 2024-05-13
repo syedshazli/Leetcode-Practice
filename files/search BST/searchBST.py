50bd55fee217f6569c9ca81c4f8257db59d305ab
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-12 23:08:36 -0400

    Documentation update

--- files/search BST/searchBST.py
+++ files/search BST/searchBST.py
@@ -1,3 +1,14 @@
+# First Binary Search Tree Problem, congratulations
+# I felt like this was similar to a binary search.
+# If the root value is bigger than the target, call the function again with it's left child which would be smaller
+#  Likewise if the root value is smaller than the target, call the function again with it's right child which would be bigger
+#  Return root if none of these are true
+#  This should all be inside a if(root!= null), after that, return Null which means the target isn't available.
+
+
+
+
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
