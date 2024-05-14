3bd37b855ebf8897a8594b8b856331b70c2bf5bd
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-13 22:02:43 -0400

    Update twoSumRedo.java

--- files/twoSum/twoSumRedo.java
+++ files/twoSum/twoSumRedo.java
@@ -1,6 +1,38 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-      //lets try to implement with a better runtime than 
-      // O(n^2) do this with hashmaps maybe?
-        }
-}
+        
+         int[] correct = new int[2];
+   
+
+//HashMap to store the number as the key and the index as the value.
+
+// for loop over numslist
+
+// return  newarray [index, index2]
+HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+int index = 0;
+int sumVal = 0;
+int index2 = 0;
+
+for(int i = 0; i<nums.length; i++){
+
+    
+   
+
+sumVal = target- nums[i];
+
+
+    if(map.containsKey(sumVal)){
+        index = map.get(sumVal);
+        index2 = i;  
+          }
+
+map.put( nums[i] , i );
+    } //end for loop
+     correct[0] = index;
+    correct[1] = index2;
+   return correct;
+    }
+  
+    
+    }
