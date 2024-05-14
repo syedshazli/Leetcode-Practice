e9162d2d9826233e54532a4c0efc935fd95cd8af
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-14 11:44:41 -0400

    Update lowestCommonAncestor.java

--- files/lowestCommonAncestor.java
+++ files/lowestCommonAncestor.java
@@ -1,1 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        /**
+        *Thoughts
+        * Will probably need to implement DFS and go until node is null as base case ?
+        * Compare with other times node is null and find the 
+        * Common ancestor means something ABOVE you, and the lowest value of the connected
+        * search for the first value that is between p and q. That will be your lowest common ancestor
+        * so is this basically a dfs of root, where root.left = p and root.right = q?
+         */
+
+            if(root != null){
+            if(root.val>p.val){return lowestCommonAncestor(root.right, p, q);}
+            else if(root.val<q.val){return lowestCommonAncestor(root.left, p, q);}
+            }
+           return root; 
+
+
+        
+
+    }
+}
