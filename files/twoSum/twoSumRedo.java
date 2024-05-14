244da71f921186ba6db88871f6b77220d4b8dd9e
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-13 22:26:02 -0400

    Update twoSumRedo.java

--- files/twoSum/twoSumRedo.java
+++ files/twoSum/twoSumRedo.java
@@ -1,3 +1,18 @@
+/**
+*Notes
+* Optimized solution this time, O(n) instead of O(n^2)
+* create a hashmap
+* loop through array, constantly updating a variable equal to "target - nums[i]" after every iteration
+* if the variable created is present in the hashmap
+*     set the start index to the map.get(variable)
+*     set the second index to the current index
+*          break to reduce runtime
+* Outside of the if, add the current value and its index to map
+* (added at the end to avoid self reference)
+
+*/
+
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
@@ -22,6 +37,7 @@ sumVal = target- nums[i];
     if(map.containsKey(sumVal)){
         correct[0] = map.get(sumVal);
         correct[1] = i; 
+        break; //reached our goal
           }
 
 map.put( nums[i] , i );//added at the end to avoid self reference
