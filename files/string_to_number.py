facf8ddfae47443a58bdf3c5d04d0d38ecc7be30
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-14 19:48:00 -0400

    Create string_to_number.py

---
+++ files/string_to_number.py
@@ -0,0 +1,6 @@
+def string_to_number(s):
+    # your code here
+    convert = int(s)
+    return convert
+    pass 
+#What makes python so desireable is methods like these, that make it easy to cast strings to ints
