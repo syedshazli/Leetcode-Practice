acfe83cb379140131ff8d5c8fe1064b63eb1c9f4
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-17 13:46:42 -0400

    Create greet.py

---
+++ files/codewars/greet.py
@@ -0,0 +1,8 @@
+#Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+#[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+#Note to self, just making sure I stay active while on vacation lol
+def greet(name):
+    #Good Luck (like you need it)
+    return("Hello, "+ name +" how are you doing today?")
+pass
