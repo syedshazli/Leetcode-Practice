cc50c32b6fc5ea61597a49e3aed1cfeb48510514
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-18 01:52:52 -0400

    Create moveZeroes.py

---
+++ files/moveZeroes.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                nums.remove(nums[i])
+                nums.append(0)
+        return nums
