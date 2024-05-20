8fb682e66963558904010f55ff28bc01f319f292
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-20 03:07:59 -0400

    Create summation.py

---
+++ files/codewars/summation.py
@@ -0,0 +1,10 @@
+#Code to find the summation of a givne number 
+def summation(num):
+    sum = 0
+    
+    for i in range(num+1):
+        sum += i
+        
+    return sum
+    pass # Code here
+    
