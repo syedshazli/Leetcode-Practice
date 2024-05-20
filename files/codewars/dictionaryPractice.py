227c9f9333fa6298de612622fb7b570f1527b41c
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-20 04:01:27 -0400

    Create dictionaryPractice.py

---
+++ files/codewars/dictionaryPractice.py
@@ -0,0 +1,24 @@
+myfamily ={
+  "child1":{
+    "name": "Hamnah",
+    "year":2003
+  },
+  
+  "child2":{
+    "name":"Syed",
+    "year": 2005
+  },
+  
+  "child3":{
+    "name": "Uzair",
+    "year": 2012
+  }
+  
+  
+}
+
+for x, obj in myfamily.items():
+    print(x)
+    
+    for y in obj:
+        print(y + ':', obj[y])
