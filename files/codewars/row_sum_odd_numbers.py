64c981784bafbe61ee8285dd24c609196240bfe1
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-27 08:28:24 -0400

    Update row_sum_odd_numbers.py

--- files/codewars/row_sum_odd_numbers.py
+++ files/codewars/row_sum_odd_numbers.py
@@ -1,3 +1,17 @@
+"""Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+
+"""
     def row_sum_odd_numbers(n):
     #your code here
     #for x in range(1, 30, 2): jumps two every iteration
