95e560aa4767eb2dde417cb681c419224e149063
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-27 08:27:47 -0400

    Update row_sum_odd_numbers.py

--- files/codewars/row_sum_odd_numbers.py
+++ files/codewars/row_sum_odd_numbers.py
@@ -1,3 +1,15 @@
-def row_sum_odd_numbers(n):
+    def row_sum_odd_numbers(n):
     #your code here
-    # this is a 7 kyu problem
+    #for x in range(1, 30, 2): jumps two every iteration
+    #sum = 0
+    #first number is blank % n = 1
+    # 4th row has 4 numbers, 5th row has 5 numbers, etc
+    
+    num = 1
+    total = 0
+    #find the first number
+    for i in range(n):
+        total = total + num
+        num = num + 2
+    total = total * n
+    return total
