134b06d6e0093ca3ae35ad1b2a42cc9f81acd26c
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-27 08:26:24 -0400

    Create sum.py

---
+++ files/codewars/sum.py
@@ -0,0 +1,15 @@
+def sum(n):
+    #your code here
+    #for x in range(1, 30, 2): jumps two every iteration
+    #sum = 0
+    #first number is blank % n = 1
+    # 4th row has 4 numbers, 5th row has 5 numbers, etc
+    
+    num = 1
+    total = 0
+    #find the first number
+    for i in range(n):
+        total = total + num
+        num = num + 2
+    total = total * n
+    return total
