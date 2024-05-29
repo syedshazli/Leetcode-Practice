7db14b4aa19098f4d32adfedc07af2b7eaef8ba7
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-29 12:07:34 -0400

    Create find_average.py

---
+++ files/codewars/find_average.py
@@ -0,0 +1,9 @@
+def find_average(numbers):
+    # your code here
+    
+    if len(numbers) != 0:
+        summation = sum(numbers)
+        return summation/len(numbers)
+    
+    return 0
+    pass
