e3666d9e90e3dc4ed50df4beaac1d76210bd87d8
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-06-04 14:46:17 -0400

    Update groupAnagrams.py

--- files/groupAnagrams.py
+++ files/groupAnagrams.py
@@ -1,1 +1,29 @@
+from collections import defaultdict
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        newMap = {}
+        resultList = []
+        newWord = ""
+       
+        for word in strs:
+            words = sorted(word)
+            words = ''.join(words)
 
+            if words not in newMap:
+
+                newMap[words] = [word]
+            else:
+                newMap[words].append(word) 
+
+        for val in newMap:
+            #obtain value from key and put it in the list of lists
+            x = newMap.get(val)
+            resultList.append(x)
+        
+        return resultList
+
+        
