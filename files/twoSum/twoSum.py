915210ab823608b920000408e1fb66babce97ba7
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-06-04 12:49:01 -0400

    Update twoSum.py

--- files/twoSum/twoSum.py
+++ files/twoSum/twoSum.py
@@ -2,16 +2,15 @@ class Solution(object):
     def twoSum(self, nums, target):
         correct = [0,0]
         newMap = dict()
-        sumVal = 0
         for i in range(len(nums)):
             sumVal = target - nums[i]
             if sumVal in newMap:
-                correct[0] = newMap[sumVal]
-                correct[1] = i
-            newMap[nums[i]] = i 
+                correct[0] = i
+                correct[1] = newMap[sumVal]
+                break
+            newMap[nums[i]] = i
+
         return correct
-
-
 
     #
     #
