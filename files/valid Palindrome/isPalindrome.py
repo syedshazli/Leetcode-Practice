62e5cd5fea12641e29409175ce455fba83f06d56
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-06-22 13:21:41 -0400

    Update isPalindrome.py

--- files/valid Palindrome/isPalindrome.py
+++ files/valid Palindrome/isPalindrome.py
@@ -1,1 +1,20 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        #first convert everything to lowercase
+        #try not to abuse python methods, and actually use 2 pointers
+        s = s.lower() #removes lowercase
+        exclude = set(string.punctuation)
+        s = ''.join(ch for ch in s if ch not in exclude)
+        s = s.replace(" ","" )
+        newString = ""
+        for i in range(len(s)-1, -1, -1):
+            newString += s[i]
 
+        if newString == s:
+            return True
+        return False
+        
