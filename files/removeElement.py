c8e9934c77e03330c623025daac8d3c71132c25b
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-06-24 23:17:50 -0400

    Create removeElement.py

---
+++ files/removeElement.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        count = 0
+        check = 0
+        #skip values that are value
+        #pointer continues to incrmeent
+        #stop at pointer that aren't val
+        #continue iterate through array, once you get to value thats val 
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[count] = nums[i]
+                count+=1
+               
+        return count
