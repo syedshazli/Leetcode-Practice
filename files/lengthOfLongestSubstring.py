4cad6e2273a3867fa524b9a725471406c57b3e91
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-06-27 23:02:52 -0400

    Create lengthOfLongestSubstring.py

---
+++ files/lengthOfLongestSubstring.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        for i in range(len(s)):
+            
