d637646db68f8a1583d5027d3e2862abd08e9b19
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-06-29 20:58:04 -0400

    Update binarySearch.py

--- files/binary search/binarySearch.py
+++ files/binary search/binarySearch.py
@@ -1,1 +1,14 @@
 binarySearch(nums,target):
+left = 0
+right = len(nums)-1
+while left < right:
+    middle = (left + right)/2
+    if( nums[middle] > nums[left]):
+        left +=1
+    elif nums[middle] < nums[right]:
+        right -=1
+    else:
+        return nums[middle]
+    
+return -1
+        
