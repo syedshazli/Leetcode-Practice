a3087d52148f1a6b9e7b524ce62b6d693f258d96
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-07-05 23:51:23 -0400

    Update lengthOfLongestSubstring.py

--- files/lengthOfLongestSubstring.py
+++ files/lengthOfLongestSubstring.py
@@ -1,8 +1,23 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        d = set()
+
+        temp = 0
+        count = 0
+
         for i in range(len(s)):
+            if s[i] not in d:
+                d.add(s[i])
+                temp += 1
+
+            else:
+                d.remove(s[i])
+                temp = 1
+
+            if temp > count:
+                count = temp
+
+                                
+        return count
             
