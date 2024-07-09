ad73de2c35b1665a8a4e2ce07006bd589af3795c
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-07-08 22:02:46 -0400

    Update lengthOfLongestSubstring.py

--- files/lengthOfLongestSubstring.py
+++ files/lengthOfLongestSubstring.py
@@ -5,18 +5,19 @@ class Solution:
 
         temp = 0
         count = 0
+        left = 0
+        
 
-        for i in range(len(s)):
-            if s[i] not in d:
-                d.add(s[i])
-                temp += 1
+        #maybe use while loop inside for loop?
+        for right in range(len(s)):
+            while s[right] in d:
+                d.remove(s[left])
+                left +=1           
 
-            else:
-                d.remove(s[i])
-                temp = 1
-
-            if temp > count:
-                count = temp
+            d.add(s[right])
+            temp += 1
+            
+            count = max(temp, right-left+1)
 
                                 
         return count
