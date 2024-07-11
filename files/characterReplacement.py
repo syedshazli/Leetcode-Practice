088333fe526de88e11e56a145fc936e70c87465c
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-07-10 22:34:43 -0400

    Update characterReplacement.py

--- files/characterReplacement.py
+++ files/characterReplacement.py
@@ -1,1 +1,19 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        hashmap = dict()
+        # sliding window... while inside a for?
+        #uses a hash table as well...
+        for i in range(len(s)):
+            hashmap[s[i]] = hashmap.get(s[i], 0) + 1 #correctly populate
 
+            
+            left = i
+            right = i+k
+            print(hashmap)
+        return 0
+        
