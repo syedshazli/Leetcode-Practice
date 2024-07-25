961f730a8bb48b17025862d6e7fb64fb03615e34
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-07-24 23:50:05 -0400

    Update binarySearch.py

--- files/binary search/binarySearch.py
+++ files/binary search/binarySearch.py
@@ -1,14 +1,25 @@
-binarySearch(nums,target):
-left = 0
-right = len(nums)-1
-while left < right:
-    middle = (left + right)/2
-    if( nums[middle] > nums[left]):
-        left +=1
-    elif nums[middle] < nums[right]:
-        right -=1
-    else:
-        return nums[middle]
-    
-return -1
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        high = len(nums)-1
+        low = 0
+        
+        while high >low:
+            
+            mid = low + (high-low)/2
+            print(mid)
+            if nums[mid] < target:
+                low=(mid+1)
+                #the middle value is too big for our target. adjust the high end to be one less
+            elif nums[mid] > target:
+                high= (mid-1)
+                # the middle value is too small for the target. increase lower bound to 1 more
+            else:
+                return mid
+        return -1
         
