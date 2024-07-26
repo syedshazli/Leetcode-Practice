39045429b6e3cde9e9033eb4beb1c663c81dea17
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-07-26 00:22:51 -0400

    Update searchMatrix.py

--- files/searchMatrix.py
+++ files/searchMatrix.py
@@ -1,4 +1,10 @@
-
+#Notes
+# A pretty good problem. Initial appraoch was to try and flatten 2D array into 1D but didnt know how to do in under O(n^2)
+# Instead, I thought myabe finding the actual low and high values would be important, but it wasn't. This helped me find the rows, columns, and max/min vals though
+# The way you calculate middle in this problem is different. MIddle here equals 1 more than our normal middle calculation
+# The crux of the problem was then figuring out how to access the rows and columns based on our middle values. We did this through here: [mid/colCount][mid%colCount]
+# Reminder to not add one to the middle count if your bounds are too low. SUbtract 1 from high count if bounds are too low
+# Good improvement
 class Solution(object):
     def searchMatrix(self, matrix, target):
         """
@@ -26,7 +32,7 @@ class Solution(object):
       
          
         while lowIndex < highIndex:
-            mid = lowIndex + (highIndex-lowIndex+1)/2
+            mid = (lowIndex + (highIndex-lowIndex+1)/2)+1
             print("mid: ", mid)
             # no. of rows = mid/c
             # no. of cols= mid%c
