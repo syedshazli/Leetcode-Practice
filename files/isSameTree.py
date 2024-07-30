ebe89a0bd37ddfea5f1a810b10b606287ef5dfde
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-07-30 00:10:56 -0400

    Update isSameTree.py

--- files/isSameTree.py
+++ files/isSameTree.py
@@ -1,17 +1,6 @@
-class Solution(object):
-    def isSameTree(self, p, q):
-        """
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: bool
-        """
-        if p is None and q is None:
-            return True
-        elif p is None and q is not None or p is not None and q is None:
-            return False
+# Intuition
+<!-- Describe your first thoughts on how to solve this problem. -->
+We want to make sure we can check if the left nodes exist for both, and if they do, are they the same value. The same thing can be said for the right nodes. If at our base case, one is none and the other isn't return false. Otherwise return true 
 
-        # if p.val != q.val and p is not None and q is not None:
-        #     return False
-
-
-        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left) and p.val == q.val
+# Approach
+Our base cases cover the situation where both nodes are none, or one contains a value and the other doesn't. So then once these are returned, if just one of the cases in the return comes up as False (such as values not being equal), the function returns false as well. So after the base cases, return the recursive solution for right, left and if val's are equal
