f4d8646636768a03d7a89278998f5dec77f25b74
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-07-30 23:32:40 -0400

    Update REDO NEEDED carFleet.py

--- files/stack/REDO NEEDED carFleet.py
+++ files/stack/REDO NEEDED carFleet.py
@@ -1,3 +1,8 @@
+# A pretty weird problem honestly all things considered
+# We combine the array of speeds and positions, and iterate through while sorting this combined array, in reverse order
+# There is a sort of mathematics to it, we must append (target-p)/s for each iteration
+# If the length of the stack is greater than 2 and the top item of the stack is less than or equal to the item before it, pop it from the stack, and return the length of the stack
+# this works because we are popping it effectivley making those 2 cars 1 fleet. they will be considered as 1 car when comparing to the next car trying to join the fleet
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         stack = []
