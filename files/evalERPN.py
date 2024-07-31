b85a2febfbb13c475ad6fae111c6540968c91201
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-07-30 23:29:11 -0400

    Update evalERPN.py

--- files/evalERPN.py
+++ files/evalERPN.py
@@ -1,3 +1,8 @@
+#Notes
+# A somewhat bruteforce solution, but could also attach all the necessary 'operations' in a set
+# If something equals any of the operations  we pop the first 2 items on the stack and either add, subtract, multiply, divide
+# Otherwise, just append the current token as an int
+#return the only item left in the stack "stack[0]"
 class Solution(object):
    
     def evalRPN(self, tokens):
