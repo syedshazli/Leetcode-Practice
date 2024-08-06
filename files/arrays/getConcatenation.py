67416e48570940073afee306082393e94c2ac3db
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-08-05 21:28:59 -0400

    Update getConcatenation.py

--- files/arrays/getConcatenation.py
+++ files/arrays/getConcatenation.py
@@ -1,3 +1,5 @@
+# we can initialize an array of size 2*n, and can reduce runtime by iterating throug length of n
+# from there we set values to ans[i] and ans[i+len(nums)] solved!
 class Solution(object):
     def getConcatenation(self, nums):
         """
@@ -9,5 +11,5 @@ class Solution(object):
     
         for i in range(len(nums)):
             ans[i] = nums[i]
-            ans[i+len(nums)] = nums[i]
+            ans[i+n] = nums[i]
         return ans
