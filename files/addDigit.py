482a99da5ec312617f59fc26c6a9d3b526ab6c8d
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-08-14 00:17:11 -0400

    Update addDigit.py

--- files/addDigit.py
+++ files/addDigit.py
@@ -1,1 +1,17 @@
 
+class Solution:
+    def addDigits(self, num: int) -> int:
+        #while loop that calls add on each iter
+        def add(num):
+            count = 0
+            for x in str(num):
+                count += int(x)
+            return count
+
+        
+        while len(str(num))>1:
+            num =add(num)
+        return num
+        
+            #do a function that will add the two digits within a for loop
+        
