cfea0e7fb47d95a6fd3136391b87a33af70c9b0e
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-08-18 18:15:22 -0400

    Update commands.py

--- files/Capital One/commands.py
+++ files/Capital One/commands.py
@@ -1,1 +1,11 @@
+# Unix commands can be executed as 
+# ls
+# cp 
+# mv
+# !1
+# !2
+# !4
+# where !index indicates which commands to execute based on the order executed.
+#   Example, !1 will execute ls, !2, will execute cp, and !4 will execute !1 which will execute ls
 
+# find the frequencies of ls, cp, mv, and add them to an array
