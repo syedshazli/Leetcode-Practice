9d7bf7413e5bea50fbcebd96ca3bcaec2d6843ca
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-08-23 21:27:29 -0400

    Update uniqueEmails.py

--- files/uniqueEmails.py
+++ files/uniqueEmails.py
@@ -1,1 +1,14 @@
-
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+        unique_emails = set()
+        
+        for email in emails:
+            local, domain = email.split('@')
+            local = local.split('+')[0].replace('.', '')
+            unique_emails.add(local + '@' + domain) #doesnt allow duplicates
+        
+        return len(unique_emails)
