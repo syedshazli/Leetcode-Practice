626d9f2a330488758010b3d18f72e0ac5dc1e1d5
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-08-27 21:16:06 -0400

    Update isMonotonic.py

--- files/isMonotonic.py
+++ files/isMonotonic.py
@@ -1,1 +1,25 @@
 
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        monoInc = True
+        monoDec = True
+
+        # need to have separate checks for monotonic increasing or decreasing
+        if len(nums) == 0 or len(nums) ==1:
+            return True
+
+        for i in range(1, len(nums)):
+            if nums[i-1]>nums[i]:
+                print(nums[i])
+                monoInc = False
+            elif nums[i-1]<nums[i]:
+                print(nums[i])
+                monoDec = False
+            else:
+                continue
+        
+        if monoInc == True or monoDec == True:
+            return True
+        else:
+            return False
+            
