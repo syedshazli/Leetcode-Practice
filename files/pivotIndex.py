c336a2fbda97d738fbf8fda9a8d7abd2e6a1b240
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-08-31 20:52:02 -0400

    Update pivotIndex.py

--- files/pivotIndex.py
+++ files/pivotIndex.py
@@ -1,1 +1,14 @@
+#brute force:
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        leftSum = 0
+        rightSum = 0
+        for i in range(len(nums)):
+            leftSum = sum(nums[:i+1])
+            print(leftSum)
+            rightSum = sum(nums[i:])
+            print(rightSum)
+            if leftSum == rightSum:
+                return i
 
+        return -1
