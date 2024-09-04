cdc9747e4c8308ad19996a6dad04a346927784c6
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-09-04 00:20:31 -0400

    Update findDisappearedNumbers.py

--- files/findDisappearedNumbers.py
+++ files/findDisappearedNumbers.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        #array nums... length of n but have n integers
+        # current element is part of [1,n]
+        # my brain knows what neess to be done.
+        # if i not in nums[i] new.append(i)
+        # numbers in range [1,n] that are not in nums
+        new = []
+        hashmap = dict()
+        for i in range(len(nums)):
+            hashmap[nums[i]] = nums[i]
+        for i in range(1, len(nums)+1):
+            if i not in hashmap:
+                new.append(i)
+        return new
+        
