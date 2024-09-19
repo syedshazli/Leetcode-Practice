b2ed23c91a844ea991b943cd00f45bbd14135b61
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-09-18 23:53:30 -0400

    Update encode.py

--- files/IBM Prep/encode & decode string/encode.py
+++ files/IBM Prep/encode & decode string/encode.py
@@ -1,1 +1,17 @@
-
+def encode(self, strs: List[str]) -> str:
+        #separate each word by a '#' character
+        #this means add a # at the end of each word
+        # also think about the last word, maybe we dont need to append there
+        #for each word in the list, add to the end of the word
+        newStr = []
+        for word in strs:
+            newStr.append(str(len(word)))
+            newStr.append('#')
+            newStr.append(word)
+            
+      
+       
+        newsString = ''.join(newStr)
+        print(newsString)
+        #return a string
+        return newsString
