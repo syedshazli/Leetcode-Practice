910408dad971e9bd78fcc9c11ca09159d24bc3f6
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-09-19 22:33:12 -0400

    Update productExceptSelf.py

--- files/IBM Prep/productExceptSelf.py
+++ files/IBM Prep/productExceptSelf.py
@@ -42,3 +42,51 @@ class Solution:
 
         #postfixLoop
         return resultArr
+
+#alternative solution w/2 for loops:
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # we know not to include the current element
+        # the reason why division helps is bc you can just:
+        #    calculate the total product for each element and store in array
+        #       for each element, append the total product divided by current element
+
+        #but this isnt possible due to constraints of problem
+        # can calculate product of whats on the left of each element 
+        # and multiply by product of whats on the right of each element
+
+        # my issue is how do we do so that it's uniquely for each value in the array?
+        prefixArr = [1] *len(nums)
+        postfixArr = [1]*len(nums)
+        resultArr = [1]*len(nums)
+
+        #prefix loop, this works!
+        currentNum = 0
+        for i in range(len(nums)):
+            if i == 0:
+                prefixArr[i] = 1
+                currentNum = 1
+            else:
+                currentNum *= nums[i-1]
+                prefixArr[i] = currentNum
+        #print(prefixArr)
+
+        #this works for postfix!
+        postfixNum = 1
+        for j in range(len(nums) - 1, -1, -1):
+            #optimized to do postfix and result in one step
+            if j != len(nums)-1:
+                postfixNum*= nums[j+1]
+                postfixArr[j]=postfixNum
+                resultArr[j] = prefixArr[j]*postfixArr[j]
+            else:
+                resultArr[j] = prefixArr[j]*postfixArr[j]
+
+        
+        
+        # for z in range(len(nums)):
+        #     resultArr[z] = prefixArr[z]*postfixArr[z]
+
+
+        #postfixLoop
+        return resultArr
