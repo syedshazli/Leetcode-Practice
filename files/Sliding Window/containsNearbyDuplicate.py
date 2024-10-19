51122e2733217eace10aff412fc89feab2a14b84
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-10-18 23:03:11 -0400

    Update containsNearbyDuplicate.py

--- files/Sliding Window/containsNearbyDuplicate.py
+++ files/Sliding Window/containsNearbyDuplicate.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # cannot involve sorting input array because we need the indices
+        # sliding window makes it so that instead of O(n^2), we do it in O(n)
+        window_set = set()
+        for i in range(len(nums)):
+            if nums[i] in window_set:
+                    return True
+            window_set.add(nums[i])
+            
+            if len(window_set)>k:
+                window_set.remove(nums[i-k])
+                
+        return False
