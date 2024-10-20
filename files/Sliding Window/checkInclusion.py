bcf5ac4f59d3c870b6af4c926491e9790dea3cde
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-10-19 22:33:54 -0400

    Update checkInclusion.py

--- files/Sliding Window/checkInclusion.py
+++ files/Sliding Window/checkInclusion.py
@@ -1,1 +1,16 @@
 
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        i = 0
+        listS1 = list(s1)
+        listS1.sort()
+
+        while i + len(s1)<=len(s2):
+            listS2 = list(s2[i:i+len(s1)])
+            listS2.sort()
+
+            if listS2 == listS1:
+                return True
+            i+=1
+        return False
+        
