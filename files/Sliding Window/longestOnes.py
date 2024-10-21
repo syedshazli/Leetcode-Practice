53dda873facc83babcb252ead5b94634c11ea893
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-10-20 23:48:51 -0400

    Update longestOnes.py

--- files/Sliding Window/longestOnes.py
+++ files/Sliding Window/longestOnes.py
@@ -1,1 +1,22 @@
 
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        left = 0
+        right = 0
+        maxSize  = 0
+        zeroCount  = 0
+
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zeroCount +=1
+                
+            while zeroCount > k:
+                if nums[left] == 0:
+                    zeroCount-=1
+                left+=1
+
+            
+
+            maxSize = max(maxSize, right-left+1)
+        return maxSize
+        
