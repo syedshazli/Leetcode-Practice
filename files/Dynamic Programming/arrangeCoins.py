10d341cd891ae29cbd33fec57be4eedb8cc606c3
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-11-14 15:01:32 -0500

    Update arrangeCoins.py

--- files/Dynamic Programming/arrangeCoins.py
+++ files/Dynamic Programming/arrangeCoins.py
@@ -1,6 +1,7 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
         # probably want to have a coin array somewhere to store previous results
+        # Note: THis is not a DP Problem but includes aspects of DP like not using redundant calculations
         newN = n
         numRows = 0
         for i in range(1,n+1):
