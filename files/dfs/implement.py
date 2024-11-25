225467e07cabeaa94618f73559b163176b53f090
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-11-25 00:36:29 -0500

    Update implement.py

--- files/dfs/implement.py
+++ files/dfs/implement.py
@@ -1,4 +1,17 @@
 # How I think it should go
 # INITIALIZATION
 # keep a list of visited nodes, initially empty
+# keep a stack, do store which items to go thru next
+# a list of neighbors
 # TRAVERSING THROUGH 
+def dfs(Graph, vertice):
+    marked = [False] * len(Graph)
+    stack = []
+    while len(stack) >0:
+      v = stack.pop()
+      
+      if not marked[v]:
+        marked[v] = True
+        for v in vertice.neighbors():
+          if not marked[v]:
+            stack.append(v)
