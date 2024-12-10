6545a5600e8c26696d20b54d215b1a2700e2bcb7
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-12-09 22:46:42 -0500

    Update invert.py

--- files/trees/invert.py
+++ files/trees/invert.py
@@ -1,1 +1,11 @@
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        #this is not a black and white dfs problem, because you basiccally have to make sure the whole subtree is inverted
+        #so you could traverse on 
