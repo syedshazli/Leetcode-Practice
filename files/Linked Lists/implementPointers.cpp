b4963a8d2b6e4bea2088827d9d0fef9d9ec79c2b
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-12-17 16:33:59 -0500

    Update implementPointers.cpp

--- files/Linked Lists/implementPointers.cpp
+++ files/Linked Lists/implementPointers.cpp
@@ -5,6 +5,8 @@ int main(){
     int find = 38;
     int *pointToFind = &find;
 
+    int **pointerToPointer = &pointToFind;
+
     cout<<"Variable has a value of "<<find<<endl;
     cout<<"Memory address of variable is "<<&find<<endl;
 
@@ -13,4 +15,7 @@ int main(){
 
     cout<<"My pointer dereferenced should have the same value as find. It's value is "<<*pointToFind<<endl;
 
+    cout<<"I made a pointer that points to PointToFind. It has a memory address of "<<pointerToPointer<<" and when dereferenced, the value is "<<*pointerToPointer<<" which is the memory address of the pointer I am pointing to"<<endl;
+
+    cout<<"To dereference the value of the pointer I am pointing to, we need to use **. So the value of what my pointer is pointing to is "<<**pointerToPointer<<endl;
 }
